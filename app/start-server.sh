67ee7928936f2e1e917efa071cfadaf0d74664fa
kamino <lishichenjs@163.com>
2019-02-15 17:04:00 +0800

    samp in docker

--- app/start-server.sh
+++ app/start-server.sh
@@ -1,23 +1,17 @@
 #!/bin/sh
 
 #check server files
-check(){
-    if [ ! -d '/app/samp03/samp03svr' ]; then
-        echo 'INSTALL SERVER'
-        tar -zxvf /app/samp*.tar.gz
-    fi
-}
+if [ ! -d '/app/samp03/samp03svr' ]; then
+    echo 'INSTALL SERVER'
+    tar -zxvf /app/samp*.tar.gz
+fi
 
 #init
-init(){
-    if [ ! -n ${RCON_PASSWORD}} ]; then
-        sed -i "s/rcon_password changeme/rcon_password ${RCON_PASSWORD}/g" /app/samp03/server.cfg
-    fi
-}
+if [ ! -n ${RCON_PASSWORD}} ]; then
+    sed -i "s/rcon_password changeme/rcon_password ${RCON_PASSWORD}/g" /app/samp03/server.cfg
+fi
 
 #start server
-check()
-init()
 echo 'START SA-MP SERVER'
 cd /app/samp03
 exec ./samp03svr
